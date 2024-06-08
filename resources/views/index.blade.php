79efd3a04384e901b7a010579f51737529657071
frankpezo <73926318+frankpezo@users.noreply.github.com>
2024-06-07 23:17:07 -0500

    Colocando archivos css y js de bootstap en la carpeta Public

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="contain">
+        <div class="row">
+            <div class="col">
+                <h1 class="text text-success">Hola</h1>
+            </div>
+        </div>
+    </div>
+@endsection
